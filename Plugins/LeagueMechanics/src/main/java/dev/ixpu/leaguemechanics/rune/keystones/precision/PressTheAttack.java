7d62eeed5cddd26ec68b102e8fb8febbd5653d11
exponent <caday.dev@gmail.com>
2026-08-04 23:01:13 +0800

    fixed onProjectileHit event not registering

--- Plugins/LeagueMechanics/src/main/java/dev/ixpu/leaguemechanics/rune/keystones/precision/PressTheAttack.java
+++ Plugins/LeagueMechanics/src/main/java/dev/ixpu/leaguemechanics/rune/keystones/precision/PressTheAttack.java
@@ -58,8 +58,8 @@ public class PressTheAttack extends BaseRune {
     public void onAttack(Player attacker, Entity target, EntityDamageByEntityEvent event) {
         triggerPressTheAttack(attacker, target, event);
     }
-    public void onProjectileHit(Player attacker, Entity target) {
-        triggerPressTheAttack(attacker, target, null);
+    public void onProjectileHit(Player attacker, Entity target, EntityDamageByEntityEvent event) {
+        triggerPressTheAttack(attacker, target, event);
     }
     private void triggerPressTheAttack(Player attacker, Entity target, EntityDamageByEntityEvent event) {
         UUID playerUUID = attacker.getUniqueId();
