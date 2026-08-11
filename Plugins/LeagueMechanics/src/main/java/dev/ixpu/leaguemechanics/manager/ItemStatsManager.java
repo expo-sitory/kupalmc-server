2bccd0609cbb2fd024562a419c51ce184a2b06e7
exponent <caday.dev@gmail.com>
2026-08-11 16:42:53 +0800

    currently have 3 starter items

--- Plugins/LeagueMechanics/src/main/java/dev/ixpu/leaguemechanics/manager/ItemStatsManager.java
+++ Plugins/LeagueMechanics/src/main/java/dev/ixpu/leaguemechanics/manager/ItemStatsManager.java
@@ -1,7 +1,6 @@
 package dev.ixpu.leaguemechanics.manager;
 
 import dev.ixpu.leaguemechanics.util.ItemStatHelper;
-
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -9,73 +8,41 @@ public class ItemStatsManager {
 
     public double getItemAP(Player player) {
         double totalAP = 0;
-        if (!ItemStatHelper.isBuildMode(player.getInventory().getItemInMainHand())) {
-            totalAP += ItemStatHelper.getStat(player.getInventory().getItemInMainHand(), "AP");
-        }
-        if (!ItemStatHelper.isBuildMode(player.getInventory().getItemInOffHand())) {
-            totalAP += ItemStatHelper.getStat(player.getInventory().getItemInOffHand(), "AP");
-        }
-
-        for (ItemStack armor : player.getInventory().getArmorContents()) {
-            if (!ItemStatHelper.isBuildMode(armor)) {
-                totalAP += ItemStatHelper.getStat(armor, "AP");
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item != null && !item.getType().isAir()) {
+                totalAP += ItemStatHelper.getStat(item, "AP");
             }
         }
-
         return totalAP;
     }
 
     public double getItemAD(Player player) {
         double totalAD = 0;
-        if (!ItemStatHelper.isBuildMode(player.getInventory().getItemInMainHand())) {
-            totalAD += ItemStatHelper.getStat(player.getInventory().getItemInMainHand(), "AD");
-        }
-        if (!ItemStatHelper.isBuildMode(player.getInventory().getItemInOffHand())) {
-            totalAD += ItemStatHelper.getStat(player.getInventory().getItemInOffHand(), "AD");
-        }
-
-        for (ItemStack armor : player.getInventory().getArmorContents()) {
-            if (!ItemStatHelper.isBuildMode(armor)) {
-                totalAD += ItemStatHelper.getStat(armor, "AD");
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item != null && !item.getType().isAir()) {
+                totalAD += ItemStatHelper.getStat(item, "AD");
             }
         }
-
         return totalAD;
     }
 
     public double getItemAR(Player player) {
         double totalAR = 0;
-        if (!ItemStatHelper.isBuildMode(player.getInventory().getItemInMainHand())) {
-            totalAR += ItemStatHelper.getStat(player.getInventory().getItemInMainHand(), "AR");
-        }
-        if (!ItemStatHelper.isBuildMode(player.getInventory().getItemInOffHand())) {
-            totalAR += ItemStatHelper.getStat(player.getInventory().getItemInOffHand(), "AR");
-        }
-
-        for (ItemStack armor : player.getInventory().getArmorContents()) {
-            if (!ItemStatHelper.isBuildMode(armor)) {
-                totalAR += ItemStatHelper.getStat(armor, "AR");
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item != null && !item.getType().isAir()) {
+                totalAR += ItemStatHelper.getStat(item, "AR");
             }
         }
-
         return totalAR;
     }
 
     public double getItemMR(Player player) {
         double totalMR = 0;
-        if (!ItemStatHelper.isBuildMode(player.getInventory().getItemInMainHand())) {
-            totalMR += ItemStatHelper.getStat(player.getInventory().getItemInMainHand(), "MR");
-        }
-        if (!ItemStatHelper.isBuildMode(player.getInventory().getItemInOffHand())) {
-            totalMR += ItemStatHelper.getStat(player.getInventory().getItemInOffHand(), "MR");
-        }
-
-        for (ItemStack armor : player.getInventory().getArmorContents()) {
-            if (!ItemStatHelper.isBuildMode(armor)) {
-                totalMR += ItemStatHelper.getStat(armor, "MR");
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item != null && !item.getType().isAir()) {
+                totalMR += ItemStatHelper.getStat(item, "MR");
             }
         }
-
         return totalMR;
     }
 }
