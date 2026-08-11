ef7c2d2d90c7d8a1da989d6ef980b777a4010d4f
exponent <caday.dev@gmail.com>
2026-08-11 21:09:55 +0800

    disabled items stat stacking

--- Plugins/LeagueMechanics/src/main/java/dev/ixpu/leaguemechanics/manager/ItemStatsManager.java
+++ Plugins/LeagueMechanics/src/main/java/dev/ixpu/leaguemechanics/manager/ItemStatsManager.java
@@ -4,13 +4,22 @@ import dev.ixpu.leaguemechanics.util.ItemStatHelper;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ItemStatsManager {
 
     public double getItemAP(Player player) {
         double totalAP = 0;
+        Set<String> countedItems = new HashSet<>();
+
         for (ItemStack item : player.getInventory().getContents()) {
             if (item != null && !item.getType().isAir()) {
-                totalAP += ItemStatHelper.getStat(item, "AP");
+                String itemId = ItemStatHelper.getItemId(item);
+                if (itemId != null && !countedItems.contains(itemId)) {
+                    totalAP += ItemStatHelper.getStat(item, "AP");
+                    countedItems.add(itemId);
+                }
             }
         }
         return totalAP;
@@ -18,9 +27,15 @@ public class ItemStatsManager {
 
     public double getItemAD(Player player) {
         double totalAD = 0;
+        Set<String> countedItems = new HashSet<>();
+
         for (ItemStack item : player.getInventory().getContents()) {
             if (item != null && !item.getType().isAir()) {
-                totalAD += ItemStatHelper.getStat(item, "AD");
+                String itemId = ItemStatHelper.getItemId(item);
+                if (itemId != null && !countedItems.contains(itemId)) {
+                    totalAD += ItemStatHelper.getStat(item, "AD");
+                    countedItems.add(itemId);
+                }
             }
         }
         return totalAD;
@@ -28,9 +43,15 @@ public class ItemStatsManager {
 
     public double getItemAR(Player player) {
         double totalAR = 0;
+        Set<String> countedItems = new HashSet<>();
+
         for (ItemStack item : player.getInventory().getContents()) {
             if (item != null && !item.getType().isAir()) {
-                totalAR += ItemStatHelper.getStat(item, "AR");
+                String itemId = ItemStatHelper.getItemId(item);
+                if (itemId != null && !countedItems.contains(itemId)) {
+                    totalAR += ItemStatHelper.getStat(item, "AR");
+                    countedItems.add(itemId);
+                }
             }
         }
         return totalAR;
@@ -38,9 +59,15 @@ public class ItemStatsManager {
 
     public double getItemMR(Player player) {
         double totalMR = 0;
+        Set<String> countedItems = new HashSet<>();
+
         for (ItemStack item : player.getInventory().getContents()) {
             if (item != null && !item.getType().isAir()) {
-                totalMR += ItemStatHelper.getStat(item, "MR");
+                String itemId = ItemStatHelper.getItemId(item);
+                if (itemId != null && !countedItems.contains(itemId)) {
+                    totalMR += ItemStatHelper.getStat(item, "MR");
+                    countedItems.add(itemId);
+                }
             }
         }
         return totalMR;
