25ef9916e2ed45d58f12482ca0529c11f0d3cca2
exponent <caday.dev@gmail.com>
2026-08-16 22:08:22 +0800

    rename

--- Plugins/LeagueMechanics/src/main/java/dev/ixpu/leaguemechanics/item/ItemStatsRegistry.java
+++ Plugins/LeagueMechanics/src/main/java/dev/ixpu/leaguemechanics/item/ItemStatsRegistry.java
@@ -1,31 +1,45 @@
 package dev.ixpu.leaguemechanics.item;
 
-public class ItemStatData {
+public class ItemStatsRegistry {
     private String id;
     private String name;
 
-    private double ad;
-    private double ap;
-    private double ar;
-    private double mr;
-
     private double hp;
     private double hr;
-    private double sr;
+
+    private double ad;
+    private double ap;
+
+    private double td;
     private double as;
+    private double ar;
+    private double mr;
+    private double ls;
+    private double cc;
+    private double sr;
+
     private double ms;
 
-    public ItemStatData(String id, String name, double ad, double ap, double ar, double mr, double hp, double hr, double sr, double as, double ms) {
+    public ItemStatsRegistry(String id, String name,
+                             double hp, double hr, double ad, double ap, double td, double as,
+                             double ar, double mr, double ls, double cc, double sr, double ms) {
         this.id = id;
         this.name = name;
-        this.ad = ad;
-        this.ap = ap;
-        this.ar = ar;
-        this.mr = mr;
+
         this.hp = hp;
         this.hr = hr;
-        this.sr = sr;
+
+        this.ad = ad;
+        this.ap = ap;
+
+        this.td = td;
         this.as = as;
+        this.ar = ar;
+        this.mr = mr;
+        this.ls = ls;
+        this.cc = cc;
+        this.sr = sr;
+
         this.ms = ms;
     }
     public String getId() {
@@ -36,42 +50,9 @@ public class ItemStatData {
         return name;
     }
 
-    public double getAd() {
-        return ad;
-    }
-
-    public void setAd(double ad) {
-        this.ad = ad;
-    }
-
-    public double getAp() {
-        return ap;
-    }
-
-    public void setAp(double ap) {
-        this.ap = ap;
-    }
-
-    public double getAr() {
-        return ar;
-    }
-
-    public void setAr(double ar) {
-        this.ar = ar;
-    }
-
-    public double getMr() {
-        return mr;
-    }
-
-    public void setMr(double mr) {
-        this.mr = mr;
-    }
-
     public double getHp() {
         return hp;
     }
-
     public void setHp(double hp) {
         this.hp = hp;
     }
@@ -79,32 +60,81 @@ public class ItemStatData {
     public double getHr() {
         return hr;
     }
-
     public void setHr(double hr) {
         this.hr = hr;
     }
 
-    public double getSr() {
-        return sr;
+
+    public double getAd() {
+        return ad;
+    }
+    public void setAd(double ad) {
+        this.ad = ad;
     }
 
-    public void setSr(double sr) {
-        this.sr = sr;
+    public double getAp() {
+        return ap;
+    }
+    public void setAp(double ap) {
+        this.ap = ap;
+    }
+
+
+    public double getTd() {
+        return td;
+    }
+    public void setTd(double td) {
+        this.td = td;
     }
 
     public double getAs() {
         return as;
     }
-
     public void setAs(double as) {
         this.as = as;
+    }
+
+    public double getAr() {
+        return ar;
+    }
+    public void setAr(double ar) {
+        this.ar = ar;
+    }
+
+    public double getMr() {
+        return mr;
+    }
+    public void setMr(double mr) {
+        this.mr = mr;
+    }
+
+    public double getLs() {
+        return ls;
+    }
+    public void setLs(double ls) {
+        this.ls = ls;
+    }
+
+    public double getCc() {
+        return cc;
+    }
+    public void setCc(double cc) {
+        this.cc = cc;
+    }
+
+    public double getSr() {
+        return sr;
+    }
+    public void setSr(double sr) {
+        this.sr = sr;
     }
 
     public double getMs() {
         return ms;
     }
-
     public void setMs(double ms) {
         this.ms = ms;
     }
+
+
 }
