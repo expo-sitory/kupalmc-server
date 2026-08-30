05e8491d119c35c059d3e9932e1148c2bd37df1d
exponent <caday.dev@gmail.com>
2026-08-30 19:55:37 +0800

    Modified

--- LeagueMechanics/src/main/java/dev/ixpu/leaguemechanics/util/RunePersistence.java
+++ LeagueMechanics/src/main/java/dev/ixpu/leaguemechanics/util/RunePersistence.java
@@ -1,5 +1,6 @@
 package dev.ixpu.leaguemechanics.util;
 
+import dev.ixpu.leaguemechanics.player.PlayerClassType;
 import dev.ixpu.leaguemechanics.rune.RunePath;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -47,6 +48,31 @@ public class RunePersistence {
         return RunePath.fromId(pathId);
     }
 
+    public void saveSecondaryPath(UUID playerUUID, RunePath path) {
+        FileConfiguration config = YamlConfiguration.loadConfiguration(dataFile);
+        String playerKey = playerUUID.toString();
+        if (path == null) {
+            config.set(playerKey + ".secondary-path", null);
+        } else {
+            config.set(playerKey + ".secondary-path", path.getId());
+        }
+        try {
+            config.save(dataFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public RunePath loadSecondaryPath(UUID playerUUID) {
+        FileConfiguration config = YamlConfiguration.loadConfiguration(dataFile);
+        String playerKey = playerUUID.toString();
+        String pathId = config.getString(playerKey + ".secondary-path");
+        if (pathId == null) {
+            return null;
+        }
+        return RunePath.fromId(pathId);
+    }
+
     public void saveKeystoneRune(UUID playerUUID, String runeId) {
         FileConfiguration config = YamlConfiguration.loadConfiguration(dataFile);
         String playerKey = playerUUID.toString();
@@ -62,6 +88,87 @@ public class RunePersistence {
         FileConfiguration config = YamlConfiguration.loadConfiguration(dataFile);
         String playerKey = playerUUID.toString();
         return config.getString(playerKey + ".keystone-rune");
+    }
+
+    public void savePlayerClass(UUID playerUUID, PlayerClassType classType) {
+        FileConfiguration config = YamlConfiguration.loadConfiguration(dataFile);
+        String playerKey = playerUUID.toString();
+        if (classType == null) {
+            config.set(playerKey + ".player-class", null);
+        } else {
+            config.set(playerKey + ".player-class", classType.getId());
+        }
+        try {
+            config.save(dataFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public PlayerClassType loadPlayerClass(UUID playerUUID) {
+        FileConfiguration config = YamlConfiguration.loadConfiguration(dataFile);
+        String playerKey = playerUUID.toString();
+        String classId = config.getString(playerKey + ".player-class");
+        if (classId == null) {
+            return null;
+        }
+        return PlayerClassType.fromId(classId);
+    }
+
+    private void saveRuneSlot(UUID playerUUID, String slotKey, String runeId) {
+        FileConfiguration config = YamlConfiguration.loadConfiguration(dataFile);
+        String playerKey = playerUUID.toString();
+        if (runeId == null) {
+            config.set(playerKey + ".rune-slots." + slotKey, null);
+        } else {
+            config.set(playerKey + ".rune-slots." + slotKey, runeId);
+        }
+        try {
+            config.save(dataFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String loadRuneSlot(UUID playerUUID, String slotKey) {
+        FileConfiguration config = YamlConfiguration.loadConfiguration(dataFile);
+        String playerKey = playerUUID.toString();
+        return config.getString(playerKey + ".rune-slots." + slotKey);
+    }
+
+    public void savePrimarySlot1Rune(UUID playerUUID, String runeId) {
+        saveRuneSlot(playerUUID, "primary-slot-1", runeId);
+    }
+    public String loadPrimarySlot1Rune(UUID playerUUID) {
+        return loadRuneSlot(playerUUID, "primary-slot-1");
+    }
+
+    public void savePrimarySlot2Rune(UUID playerUUID, String runeId) {
+        saveRuneSlot(playerUUID, "primary-slot-2", runeId);
+    }
+    public String loadPrimarySlot2Rune(UUID playerUUID) {
+        return loadRuneSlot(playerUUID, "primary-slot-2");
+    }
+
+    public void savePrimarySlot3Rune(UUID playerUUID, String runeId) {
+        saveRuneSlot(playerUUID, "primary-slot-3", runeId);
+    }
+    public String loadPrimarySlot3Rune(UUID playerUUID) {
+        return loadRuneSlot(playerUUID, "primary-slot-3");
+    }
+
+    public void saveSecondarySlot1Rune(UUID playerUUID, String runeId) {
+        saveRuneSlot(playerUUID, "secondary-slot-1", runeId);
+    }
+    public String loadSecondarySlot1Rune(UUID playerUUID) {
+        return loadRuneSlot(playerUUID, "secondary-slot-1");
+    }
+
+    public void saveSecondarySlot2Rune(UUID playerUUID, String runeId) {
+        saveRuneSlot(playerUUID, "secondary-slot-2", runeId);
+    }
+    public String loadSecondarySlot2Rune(UUID playerUUID) {
+        return loadRuneSlot(playerUUID, "secondary-slot-2");
     }
 
     public void clearAllRunes(UUID playerUUID) {
