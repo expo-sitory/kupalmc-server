f26b6b22d071814f7f84cee23640cbb4ef69e7a5
exponent <caday.dev@gmail.com>
2026-09-04 15:42:42 +0800

    Register KDA placeholder

--- LeagueMechanics/src/main/java/dev/ixpu/leaguemechanics/placeholder/PlaceholderRegistry.java
+++ LeagueMechanics/src/main/java/dev/ixpu/leaguemechanics/placeholder/PlaceholderRegistry.java
@@ -65,6 +65,8 @@ public class PlaceholderRegistry extends PlaceholderExpansion {
             case "rune_path" -> getRunePathPlaceholder(player);
             case "class"      -> getClassPlaceholder(player);
 
+R            case "kda" -> dev.ixpu.leaguemechanics.player.PlayerKDA.getInstance().getFormattedKDA(player.getUniqueId());
+
             case "line1" -> getLine1(player, stats);
             case "line2" -> getLine2(player, stats);
             case "line3" -> getLine3(player, stats);
